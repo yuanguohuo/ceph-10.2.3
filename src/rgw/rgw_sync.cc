355eaed24fdeb833930907fe720aeb88cc379a09
Yuanguo <yuanguo_huo@163.com>
2017-03-30 15:26:26 +0800

    merge fix of BUG #17044: jewel: rgw: multisite: assert(next) failed in RGWMetaSyncCR

--- src/rgw/rgw_sync.cc
+++ src/rgw/rgw_sync.cc
@@ -1672,6 +1672,7 @@ class RGWMetaSyncCR : public RGWCoroutine {
   using StackRef = boost::intrusive_ptr<RGWCoroutinesStack>;
   using RefPair = std::pair<ControlCRRef, StackRef>;
   map<int, RefPair> shard_crs;
+  int ret{0};
 
 public:
   RGWMetaSyncCR(RGWMetaSyncEnv *_sync_env, RGWPeriodHistory::Cursor cursor,
@@ -1681,7 +1682,6 @@ public:
       cursor(cursor), sync_status(_sync_status) {}
 
   int operate() {
-    int ret = 0;
     reenter(this) {
       // loop through one period at a time
       for (;;) {
@@ -1733,8 +1733,13 @@ public:
             shard_crs[shard_id] = RefPair{cr, stack};
           }
         }
+
         // wait for each shard to complete
-        collect(&ret, NULL);
+        while (ret == 0 && num_spawned() > 0) {
+          yield wait_for_child();
+          collect(&ret, nullptr);
+        }
+
         drain_all();
         {
           // drop shard cr refs under lock
