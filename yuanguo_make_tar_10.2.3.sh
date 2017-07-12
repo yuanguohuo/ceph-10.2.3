e15f2c9519fa8117a0b185652c036511f0336ede
Yuanguo <yuanguo_huo@163.com>
2017-07-12 09:34:49 +0800

    add yuanguo_make_tar_10.2.3.sh and rename check-make-warnings.hyg.sh to yuanguo_check-make-warnings.sh

---
+++ yuanguo_make_tar_10.2.3.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+DIR=ceph-10.2.3-install
+TGZ=$DIR.tar.gz
+
+rm -fr $DIR $TGZ
+mkdir $DIR
+
+cp RPMS/x86_64/ceph-10.2.3-0.el7.centos.x86_64.rpm              $DIR
+cp RPMS/x86_64/ceph-base-10.2.3-0.el7.centos.x86_64.rpm         $DIR
+cp RPMS/x86_64/ceph-common-10.2.3-0.el7.centos.x86_64.rpm       $DIR
+cp RPMS/x86_64/ceph-mds-10.2.3-0.el7.centos.x86_64.rpm          $DIR
+cp RPMS/x86_64/ceph-mon-10.2.3-0.el7.centos.x86_64.rpm          $DIR
+cp RPMS/x86_64/ceph-osd-10.2.3-0.el7.centos.x86_64.rpm          $DIR
+cp RPMS/x86_64/ceph-radosgw-10.2.3-0.el7.centos.x86_64.rpm      $DIR
+cp RPMS/x86_64/ceph-selinux-10.2.3-0.el7.centos.x86_64.rpm      $DIR
+cp RPMS/x86_64/libcephfs1-10.2.3-0.el7.centos.x86_64.rpm        $DIR
+cp RPMS/x86_64/librados2-10.2.3-0.el7.centos.x86_64.rpm         $DIR
+cp RPMS/x86_64/libradosstriper1-10.2.3-0.el7.centos.x86_64.rpm  $DIR
+cp RPMS/x86_64/librbd1-10.2.3-0.el7.centos.x86_64.rpm           $DIR
+cp RPMS/x86_64/librgw2-10.2.3-0.el7.centos.x86_64.rpm           $DIR
+cp RPMS/x86_64/python-cephfs-10.2.3-0.el7.centos.x86_64.rpm     $DIR
+cp RPMS/x86_64/python-rados-10.2.3-0.el7.centos.x86_64.rpm      $DIR
+cp RPMS/x86_64/python-rbd-10.2.3-0.el7.centos.x86_64.rpm        $DIR
+cp RPMS/x86_64/librados2-devel-10.2.3-0.el7.centos.x86_64.rpm   $DIR
+#cp RPMS/x86_64/ceph-debuginfo-10.2.3-0.el7.centos.x86_64.rpm    $DIR
+
+cp install.sh                                                   $DIR
+
+tar czvf $TGZ $DIR
+rm -fr $DIR
